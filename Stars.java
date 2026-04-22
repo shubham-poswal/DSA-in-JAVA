6670e0627d0e36d39ca0cf07635616f86953bd54
shubham-poswal <145015617+shubham-poswal@users.noreply.github.com>
2026-04-22 17:11:27 +0530

    Add Stars class to print square of asterisks
    This program prints a square of asterisks of size n.

---
+++ Stars.java
@@ -0,0 +1,18 @@
+public class Stars {
+    public static void main(String[] args) {
+
+        int n = 4;
+        for (int row = 1; row <= n; row++){
+            for(int col = 1; col <= n; col++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+
+}
+//output
+// * * * * 
+// * * * * 
+// * * * * 
+// * * * * 
