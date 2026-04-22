55ba6a9ecf37b78976a401247a4172389d8202a5
shubham-poswal <145015617+shubham-poswal@users.noreply.github.com>
2026-04-22 17:28:10 +0530

    Implement right-angled triangle pattern in Java
    Add a program to print a right-angled triangle pattern using asterisks.

---
+++ ight-angled triangle pattern .java
@@ -0,0 +1,18 @@
+int n = 5;
+for(int row=1; row<=n; row++){
+//for each row -> variable columns
+// formula -> col-> 1 -> value of row
+
+for(int col=1; col<=row; col++){
+System.out.print("* ");
+}
+//move to next line.
+System.Out.println();
+}
+
+// OutPut
+// * 
+// * * 
+// * * * 
+// * * * * 
+// * * * * *
